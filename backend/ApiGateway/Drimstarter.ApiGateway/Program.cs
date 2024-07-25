7600f4fa823caac7e7c1add9812fecb6082be4b7
Dmitriy Melnik <mitro@Dmitriys-MacBook-Pro.local>
2024-07-25 09:50:18 +0500

    Create .NET Aspire solution

---
+++ backend/ApiGateway/Drimstarter.ApiGateway/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
