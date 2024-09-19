8e71b249f5a3adba47b606a8824a5d2b7cf08e53
mitro <dmitriy.melnik@drim.dev>
2024-09-19 12:48:01 +0500

    Added CryptoPayments client to ApiGateway

--- backend/Api/ApiGateway/Drimstarter.ApiGateway/Features/CryptoPayments/Requests/GetDepositAddress.cs
+++ backend/Api/ApiGateway/Drimstarter.ApiGateway/Features/CryptoPayments/Requests/GetDepositAddress.cs
@@ -1,4 +1,5 @@
 using Drimstarter.ApiGateway.Features.CryptoPayments.Models;
+using Drimstarter.BlockchainService.Client;
 using Drimstarter.Common.Web.Endpoints;
 using Microsoft.AspNetCore.Http.HttpResults;
 
@@ -12,19 +13,19 @@ public static class GetDepositAddress
 
         public void MapEndpoint(WebApplication app)
         {
-            // app.MapPost(Path, async Task<Created<DepositAddressModel>> (
-            //     Body body,
-            //     BlockchainService.Client.CryptoPaymentsClient cryptoPaymentsClient,
-            //     CancellationToken cancellationToken) =>
-            // {
-            //     var request = new GetDepositAddressRequest
-            //     {
-            //         Name = body.Name
-            //     };
-            //     var reply = await cryptoPaymentsClient.GetDepositAddressAsync(request, cancellationToken: cancellationToken);
-            //     var depositAddress = new DepositAddressModel(reply.AddressId);
-            //     return TypedResults.Created($"{Path}/{depositAddress.AddressId}", depositAddress);
-            // });
+            app.MapPost(Path, async Task<Created<DepositAddressModel>> (
+                Body body,
+                Blockchain.BlockchainClient cryptoPaymentsClient,
+                CancellationToken cancellationToken) =>
+            {
+                var request = new GetDepositAddressRequest
+                {
+                    User = "1",
+                };
+                var reply = await cryptoPaymentsClient.GetDepositAddressAsync(request, cancellationToken: cancellationToken);
+                var depositAddress = new DepositAddressModel(reply.Address);
+                return TypedResults.Created($"{Path}/{depositAddress.AddressId}", depositAddress);
+            });
         }
     }
 
